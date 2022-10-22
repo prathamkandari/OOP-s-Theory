b2e8fa2eaaca31fdff27edb189663384738674c5
prathamkandari <prathamkandari123@gmail.com>
2022-10-22 21:07:59 +0530

    Without Synchronization Showing Inconsistency

---
+++ ThreadSynchronizationInconsistent.java
@@ -0,0 +1,55 @@
+class Table
+{
+     void printTable(int n)
+    {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println(n*i);
+        }
+        try
+        {
+            Thread.sleep(500);
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
+
+class MyThread1 extends Thread
+{
+    Table t;            //Instance Varible or reference Variable
+    MyThread1(Table t)
+    {
+        this.t=t;
+    }
+    public void run()
+    {
+        t.printTable(5);
+    }
+}
+
+class MyThread2 extends Thread
+{
+    Table t; //Instance Varible or reference Variable
+    MyThread2(Table t)
+    {
+        this.t=t;
+    }
+    public void run()
+    {
+        t.printTable(10);
+    }
+}
+
+class ThreadSynchronizationInconsistent
+{
+    public static void main(String[] args) {
+        Table obj = new Table();
+        MyThread1 t1 = new MyThread1(obj);
+        MyThread2 t2 = new MyThread2(obj);
+        t1.start();
+        t2.start();
+    }
+}
